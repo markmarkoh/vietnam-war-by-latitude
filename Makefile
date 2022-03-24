609fa62fae9a371bddfd8c7d06f44b65746fd50d
Mark DiMarco <mark.dimarco@gmail.com>
2022-03-24 12:09:16 -0500

    more progress

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 clear_no_coords:
-	awk -F, '$$9 != "" && $$40 != "NONKINETIC" {print}' thor_data_vietnam_datefix.csv > thor_filtered.csv
+	awk -F, '$$9 != "" {print}' thor_data_vietnam_datefix.csv > thor_filtered.csv
 
 test:
 	Rscript process.r thor_test_example.csv thor_test_results_actual.csv;
--- process.r
+++ process.r
@@ -11,12 +11,13 @@ data = read.csv(args[1])
 
 grouped <- data %>%
   filter(TGTCOUNTRY == "SOUTH VIETNAM" | TGTCOUNTRY == "NORTH VIETNAM") %>%
-  mutate(week = lubridate::round_date(ymd(MSNDATE), unit = "week", week_start=0)) %>%
+  # drop_na(TGTLATDD_DDD_WGS84) %>% # faster to do this with awk
+  # mutate(week = lubridate::round_date(ymd(MSNDATE), unit = "week", week_start=0)) %>%
   mutate(month = lubridate::round_date(ymd(MSNDATE), unit = "month")) %>%
   mutate(lat = round(TGTLATDD_DDD_WGS84 / .5) * .5) %>%
   group_by(month, lat) %>%
   tally() %>%
-  mutate(n_3r = rollmean(n, k = 3, fill = n)) %>%
+  # mutate(n_3r = rollmean(n, k = 3, fill = n)) %>%
   print(n = 50)
 
 if (!is.null(args[2])) {
